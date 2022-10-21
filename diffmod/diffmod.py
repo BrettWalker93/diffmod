673594adf8811136f570d45f1e55ab22cbef8b99
BrettWalker93 <brettwalker1337@gmail.com>
2022-10-21 00:28:09 -0500

    first commit

--- diffmod/diffmod.py
+++ diffmod/diffmod.py
@@ -1,1 +1,27 @@
+import math
+from inspect import isfunction
+from functools import partial
 
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+from einops import rearrange
+
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+
+import attention
+import blocks
+import forwarddiffuse
+import resid
+import PreNorm
+import SinusoidalPositionEmbeddings
+import Unet
+import extract
+
+from PIL import Image
+import requests
+
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+image
